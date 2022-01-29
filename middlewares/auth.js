bd535337bf71c7f51fb7f116b277f3f544817793
Alamin Hossain <86642366+AlaminMJ@users.noreply.github.com>
2022-01-29 15:13:43 +0600

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,18 @@
 //
 const auth = {};
-auth.admin = async (req, res, next) => {};
-auth.store = async (req, res, next) => {};
-auth.canteen = async (req, res, next) => {};
+auth.admin = async (req, res, next) => {
+  const accessToken = req.headers.authoraization;
+  if (!accessToken || !accessToken.startWith("bearer")) {
+    return next("");
+  }
+  const token = accessToken.split(" ")[1];
+  const 
+};
+
+auth.store = async (req, res, next) => {
+
+};
+auth.canteen = async (req, res, next) => {
+
+};
 export default auth;
