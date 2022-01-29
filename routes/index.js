57c1f5aff6bfafb8da842bd1dbfe4f2ff2297103
Alamin Hossain <86642366+AlaminMJ@users.noreply.github.com>
2022-01-29 11:40:37 +0600

    add more routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   productListControler,
+  productController,
   userController,
   billController,
   returnController,
@@ -15,10 +16,39 @@ router.get("/", (req, res, next) => {
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 // productList
-router.post("/products", productListControler.addProduct);
-router.delete("/products/:id", productListControler.removeProduct);
-router.patch("/products/:id", productListControler.updateProduct);
-router.get("/products/:id", productListControler.oneProduct);
-router.get("/products", productListControler.allProduct);
-
+router.post("/productlists", productListControler.addProduct);
+router.delete("/productlists/:id", productListControler.removeProduct);
+router.patch("/productlists/:id", productListControler.updateProduct);
+router.get("/productlists/:id", productListControler.oneProduct);
+router.get("/productlists", productListControler.allProduct);
+// product
+router.post("/products", productController.addProduct);
+router.delete("/products", productController.addProduct);
+router.patch("/products", productController.addProduct);
+router.get("/products", productController.addProduct);
+router.get("/products", productController.addProduct);
+// return
+router.post("/returns", returnController.add);
+router.delete("/returns", returnController.add);
+router.put("/returns", returnController.add);
+router.get("/returns", returnController.add);
+router.get("/returns", returnController.add);
+// requsiton
+router.post("/requsitions", requsitionController.add);
+router.delete("/requsitions", requsitionController.add);
+router.put("/requsitions", requsitionController.add);
+router.get("/requsitions", requsitionController.add);
+router.get("/requsitions", requsitionController.add);
+// bill
+router.post("/bills", billController.add);
+router.delete("/bills", billController.add);
+router.put("/bills", billController.add);
+router.get("/bills", billController.add);
+router.get("/bills", billController.add);
+// manpower
+router.post("/manpowers", manpowerController.add);
+router.delete("/manpowers", manpowerController.add);
+router.put("/manpowers", manpowerController.add);
+router.get("/manpowers", manpowerController.add);
+router.get("/manpowers", manpowerController.add);
 export default router;
