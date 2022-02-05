b8344488730b5fc16c3bf2430c63e4c13294d716
Alamin Hossain <86642366+AlaminMJ@users.noreply.github.com>
2022-02-05 18:47:01 +0600

    add multer

--- controllers/productList.js
+++ controllers/productList.js
@@ -1,28 +1,61 @@
 import joi from "joi";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 import { ProductList } from "../models";
 const productListControler = {};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`;
+    // 3746674586-836534453.png
+    cb(null, uniqueName);
+  }
+});
+
+const handleMultipartData = multer({
+  storage,
+  limits: { fileSize: 1000000 * 5 }
+}).single("image"); // 5mb
+
 // add product
 productListControler.add = async (req, res, next) => {
   //  validation product data
-
+  handleMultipartData(req, res, (error) => {
+    if (error) {
+      return next(CustomErrorHandler.serverError(err.message));
+    }
+    const filePath = req.file.path;
+  
   const productListSchema = joi.object({
     name: joi.string().required(),
-    imgurl: joi.string().required(),
-    unit: joi.string().required(),
+    unit: joi.string().required()
   });
   const { error } = productListSchema.validate(req.body);
   if (error) {
+    // Delete the uploaded file
+    fs.unlink(`${appRoot}/${filePath}`, (err) => {
+      if (err) {
+        return next(CustomErrorHandler.serverError(err.message));
+      }
+    });
+
     return next(error);
+    // rootfolder/uploads/filename.png
   }
   const { name, imgurl, unit } = req.body;
-  const newProductList = new ProductList({ name, imgurl, unit });
+  const newProductList = new ProductList({ name, imgurl: filePath, unit });
   try {
     const result = await newProductList.save();
     return res.status(200).json(result);
   } catch (error) {
     return next(error);
-  }
+  }});
 };
+
 // delete product
 productListControler.delete = async (req, res, next) => {
   const { id } = req.params;
@@ -40,7 +73,7 @@ productListControler.update = async (req, res, next) => {
   const productSchema = joi.object({
     name: joi.string(),
     imgurl: joi.string(),
-    unit: joi.string(),
+    unit: joi.string()
   });
   const { error } = productSchema.validate(req.body);
   if (error) {
@@ -51,7 +84,7 @@ productListControler.update = async (req, res, next) => {
     const result = await ProductList.findByIdAndUpdate(
       id,
       {
-        $set: { name, imgurl, unit },
+        $set: { name, imgurl, unit }
       },
       { new: true }
     );
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import { PORT } from "./config";
 import cors from "cors";
+import path from "path";
 import mainRoute from "./routes";
 import conn from "./db/conn";
 import { errorHandler } from "./middlewares";
 const app = express();
 conn();
 // middlewares
+
 app.use(express.json());
 app.use(cors());
 app.use("/api", mainRoute);
-
+global.appRoot = path.resolve(__dirname);
 // error middleware
 app.use(errorHandler);
 // Server Listing
