0ffbeb4f124e0e22708bb5a2b83f60871dea91df
Alamin <alamin.mj.3228@gmail.com>
2022-02-12 20:53:31 +0600

    push
    Signed-off-by: Alamin <alamin.mj.3228@gmail.com>

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(errorHandler);
 app.get("/", (req, res) => {
   res.send("ok vai");
 });
-app.listen(process.env.PORT || 5000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
   console.log(`Server is runing on ${PORT}`);
 });
 //
