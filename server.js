b1c5f8ee2b20ad2d9ec0c0ce70b2eb76f9f8e7ee
Alamin <alamin.mj.3228@gmail.com>
2022-02-12 21:22:22 +0600

    ok
    Signed-off-by: Alamin <alamin.mj.3228@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import mainRoute from "./routes";
 import conn from "./db/conn";
 import { errorHandler, varifyToken } from "./middlewares";
@@ -13,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api", mainRoute);
-global.appRoot = path.resolve(__dirname);
+
 // error middleware
 app.use(errorHandler);
 // app.use(varifyToken);
